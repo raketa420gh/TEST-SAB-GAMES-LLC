777c979eb081be20e9160fe24abfda6cf558b59a
Andrei Raketa <75320766+raketa420gh@users.noreply.github.com>
2022-09-27 21:07:03 +0300

    [B-3] Unit base added

--- Assets/Content/Scripts/Unit.cs
+++ Assets/Content/Scripts/Unit.cs
@@ -1,6 +1,26 @@
+using System;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    
+    [SerializeField] private UnitData _data;
+    [SerializeField] private MeshRenderer _meshRenderer;
+
+    private int _maxHealth;
+    private float _moveSpeed;
+    private float _attackPower;
+    private Color _color;
+
+    private void Awake()
+    {
+        _maxHealth = _data.MaxHealth;
+        _moveSpeed = _data.MoveSpeed;
+        _attackPower = _data.AttackPower;
+        _color = _data.Color;
+    }
+
+    private void Start()
+    {
+        _meshRenderer.material.color = _color;
+    }
 }
